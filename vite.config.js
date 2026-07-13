b657b1ad73483cb0391ccd51f5bdb818d5ffeef0
Dilshod <axmatqulovdilshod@gmail.com>
2026-07-13 14:31:09 +0500

    fix: vite base for hashrouter

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Arlot-Ai/',     // Repository nomingiz
+  base: '/Arlot-Ai/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
